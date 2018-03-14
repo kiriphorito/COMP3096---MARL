b92acd37b7467321439219a5c20b81ca58f1de41
Raymond Tan <now-raymond@users.noreply.github.com>
2018-03-14 22:21:48 +0000

    Add framework support for taking multiple actions in a single timestep.

--- pysc2/pysc2/env/run_loop.py
+++ pysc2/pysc2/env/run_loop.py
@@ -1,3 +1,15 @@
+#================================
+# RESEARCH GROUP PROJECT [RGP]
+#================================
+# This modified file is part of the COMP3096 Research Group Project.
+# Changes are marked with `RGP`.
+#
+# Summary of changes:
+# - For agents inheriting from the MultistepAgent base class, the `multistep` function is called to obtain the array of
+# actions that the agent would like to take.
+# - The environment is stepped forward with both 'standard actions' (single actions returned from non multi-step agents)
+# and multistep actions.
+
 # Copyright 2017 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,6 +31,8 @@ from __future__ import print_function
 
 import time
 
+# RGP
+from pysc2.agents import multistep_agent
 
 def run_loop(agents, env, max_frames=0):
   """A run loop to have agents and an environment interact."""
@@ -37,13 +51,26 @@ def run_loop(agents, env, max_frames=0):
         a.reset()
       while True:
         total_frames += 1
+
+        # RGP {
+        standard_agents = [agent for agent in agents if not isinstance(agent, multistep_agent.MultistepAgent)]
+        multiagent_agents = [agent for agent in agents if isinstance(agent, multistep_agent.MultistepAgent)]
+
+        # Standard actions
         actions = [agent.step(timestep)
-                   for agent, timestep in zip(agents, timesteps)]
+                   for agent, timestep in zip(standard_agents, timesteps)]
+
+        # Multistep actions
+        multiagent_actions_list = [agent.multistep(timestep) for agent, timestep in zip(multiagent_agents, timesteps)] # list of lists
+        multiagent_actions = [action for sublist in multiagent_actions_list for action in sublist] # flatten the list to get a list of actions
+
+        # } RGP
+
         if max_frames and total_frames >= max_frames:
           return
         if timesteps[0].last():
           break
-        timesteps = env.step(actions)
+        timesteps = env.step(actions + multiagent_actions) # RGP: (+ multiagent_actions)
   except KeyboardInterrupt:
     pass
   finally:

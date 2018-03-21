eabbc8d9dad9754a9d360bddde1b13c263f09a2c
Raymond Tan <now-raymond@users.noreply.github.com>
2018-03-21 22:48:52 +0000

    [DQN] Stack frames (solves memory issues) and provide default hyperparameters

--- sc2g/sc2g/agents/dqn_agent.py
+++ sc2g/sc2g/agents/dqn_agent.py
@@ -14,6 +14,7 @@ from absl.flags import FLAGS
 from sc2g.env.sc2gym import make_env
 # Algorithm
 from baselines import deepq
+from baselines.common.atari_wrappers import FrameStack
 
 
 def train():
@@ -26,6 +27,8 @@ def train():
         visualize=FLAGS.visualize,
     )
 
+    env = FrameStack(env, 4)
+
     model = deepq.models.cnn_to_mlp(
         convs=[(32, 8, 4), (64, 4, 2), (64, 3, 1)],
         hiddens=[256],
@@ -35,17 +38,18 @@ def train():
     act = deepq.learn(
         env,
         q_func=model,
-        # lr=1e-5,
-        # max_timesteps=10000000,
-        # buffer_size=50000,
-        # exploration_fraction=0.5,
-        # exploration_final_eps=0.01,
-        # train_freq=4,
-        print_freq=10,
-        learning_starts=1000,
-        # target_network_update_freq=500,
-        # gamma=1.0,
-        # prioritized_replay=True,
+        lr=1e-5,                         # 5e-4: learning rate for adam optimizer
+        max_timesteps=5000000,           # 100000
+        buffer_size=100000,              # 50000
+        exploration_fraction=0.5,        # 0.1
+        exploration_final_eps=0.01,      # 0.02
+        train_freq=4,                    # 1: how often the model is updated, in steps
+        print_freq=10,                   # 100: how often training progress is printed, in episodes
+        checkpoint_freq=10000,           # 10000: how often to save the model, in steps
+        learning_starts=100000,          # 1000: how many steps before learning starts
+        gamma=0.99,                      # 1.0: discount factor
+        target_network_update_freq=500,  # 500: how often the target network is updated
+        prioritized_replay=True,
     )
 
     try:

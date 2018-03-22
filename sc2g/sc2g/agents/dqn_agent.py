49fb020d2d7f0959a1ac801e0b9430d605946de3
Raymond Tan <now-raymond@users.noreply.github.com>
2018-03-22 00:52:49 +0000

    [DQN] Hyperparameters can now be tuned via flags

--- sc2g/sc2g/agents/dqn_agent.py
+++ sc2g/sc2g/agents/dqn_agent.py
@@ -19,6 +19,7 @@ from baselines.common.atari_wrappers import FrameStack
 
 def train():
     FLAGS(sys.argv)
+    print(sys.argv)
 
     env = make_env(
         map_name=FLAGS.map_name,
@@ -38,18 +39,18 @@ def train():
     act = deepq.learn(
         env,
         q_func=model,
-        lr=1e-5,                         # 5e-4: learning rate for adam optimizer
-        max_timesteps=5000000,           # 100000
-        buffer_size=100000,              # 50000
-        exploration_fraction=0.5,        # 0.1
-        exploration_final_eps=0.01,      # 0.02
-        train_freq=4,                    # 1: how often the model is updated, in steps
-        print_freq=10,                   # 100: how often training progress is printed, in episodes
-        checkpoint_freq=10000,           # 10000: how often to save the model, in steps
-        learning_starts=100000,          # 1000: how many steps before learning starts
-        gamma=0.99,                      # 1.0: discount factor
-        target_network_update_freq=500,  # 500: how often the target network is updated
-        prioritized_replay=True,
+        lr=FLAGS.learning_rate,                 # Learning rate for adam optimizer
+        max_timesteps=FLAGS.max_timesteps,      # Max timesteps
+        buffer_size=FLAGS.buffer_size,          # Size of replay buffer
+        exploration_fraction=FLAGS.exploration_fraction,    # Fraction of max_timesteps over which exploration rate is annealed
+        exploration_final_eps=FLAGS.exploration_final_eps,  # Final value of random action probability
+        train_freq=FLAGS.train_freq,            # How often the model is updated, in steps
+        print_freq=FLAGS.print_freq,            # How often training progress is printed, in episodes
+        checkpoint_freq=FLAGS.checkpoint_freq,  # How often to save the model, in steps
+        learning_starts=FLAGS.learning_starts,  # How many steps before learning starts
+        gamma=FLAGS.gamma,                      # Discount factor
+        target_network_update_freq=FLAGS.target_network_update_freq,  # How often the target network is updated
+        prioritized_replay=FLAGS.prioritized_replay,
     )
 
     try:
@@ -59,17 +60,34 @@ def train():
         print("Error saving model.")
 
     print("Saving replay...")
-    env.save_replay(FLAGS.map_name)
+    env.unwrapped.save_replay(FLAGS.map_name)
 
     print("Closing environment...")
     env.close()
 
 
 def main():
-    flags.DEFINE_string("map_name", "CollectMineralShards", "Name of the map")
-    flags.DEFINE_integer("screen_size", 84, "Feature screen size")
-    flags.DEFINE_integer("minimap_size", 64, "Feature minimap size")
-    flags.DEFINE_bool("visualize", False, "Show python visualisation")
+    # Common
+    flags.DEFINE_string("map_name", "CollectMineralShards",         "Name of the map")
+    flags.DEFINE_integer("screen_size",                 84,         "Feature screen size")
+    flags.DEFINE_integer("minimap_size",                64,         "Feature minimap size")
+    flags.DEFINE_bool("visualize",                      False,      "Show python visualisation")
+
+    # Algo-specific settings
+    flags.DEFINE_integer("print_freq",                  10,         "How often training progress is printed, in episodes")  # 100
+    flags.DEFINE_integer("checkpoint_freq",             10000,      "How often to save the model, in steps")  # 10000
+
+    # Algo hyperparameters
+    flags.DEFINE_float("learning_rate",                 1e-5,       "Learning rate for adam optimizer") # 5e-4
+    flags.DEFINE_integer("max_timesteps",               2000000,    "Max timesteps")  # 100000
+    flags.DEFINE_integer("buffer_size",                 100000,     "Size of replay buffer")  # 50000
+    flags.DEFINE_float("exploration_fraction",          0.5,        "Fraction of max_timesteps over which exploration rate is annealed")  # 0.1
+    flags.DEFINE_float("exploration_final_eps",         0.01,       "Final value of random action probability")  # 0.02
+    flags.DEFINE_integer("train_freq",                  4,          "How often the model is updated, in steps")  # 1
+    flags.DEFINE_integer("learning_starts",             100000,     "How many steps before learning starts")  # 1000
+    flags.DEFINE_float("gamma",                         0.99,       "Discount factor")  # 1.0
+    flags.DEFINE_integer("target_network_update_freq",  500,        "How often the target network is updated")  # 500
+    flags.DEFINE_bool("prioritized_replay",             True,       "Whether prioritized replay is used")  # True
 
     train()
 
